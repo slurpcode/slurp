b89b7e1b9b2053ad5e8ea005ac7ba0b69a8ef743
John Bampton <jbampton@users.noreply.github.com>
2018-10-05 03:58:18 +1000

    Add effects

---
+++ assets/js/eggs.js
@@ -0,0 +1,24 @@
+function eggs() {
+    var easterEggs = ['reset', 'king', 'winter', 'digger', 'matrix'];
+    var keyHistory = '';
+    var match;
+    $(document).keypress(function (e) {
+        keyHistory += String.fromCharCode(e.which);
+        for (let i = 0; i < easterEggs.length; i++) {
+            var eggLength = easterEggs[i].length;
+            match = keyHistory.match(easterEggs[i]);
+            if (match) {
+                //alert(match);
+                var url = "/assets/images/backgrounds/" + match + ".jpg";
+                if(match.toString()==='reset'){
+                    $("main").css("background-image", "none");
+                }else{
+                    $("main").css("background-image", "url(" + url + ")");
+                }
+                keyHistory = match = '';
+            } else if (keyHistory.length > 30) {
+                keyHistory = keyHistory.substr((keyHistory.length - eggLength - 1));
+            }
+        }
+    });
+}
