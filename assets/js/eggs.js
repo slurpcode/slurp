3c4a166f36aa7a659cc0d8eee33f1fcc0d597000
John Bampton <jbampton@users.noreply.github.com>
2018-10-08 10:44:48 +1000

    Refactor and add snow

--- assets/js/eggs.js
+++ assets/js/eggs.js
@@ -2,20 +2,27 @@ function eggs() {
     var easterEggs = ['reset', 'fuchsia', 'king', 'winter', 'digger', 'matrix', 'jurassic', 'christmas', 'easter', 'harley', 'jorge', 'barney', 'karine', 'jedi', 'anjuna', 'asot', 'progressive', 'chill', 'jazz'];
     var keyHistory = '';
     var match;
+    var winter = false;
     $(document).keypress(function (e) {
         keyHistory += String.fromCharCode(e.which);
-        for (let i = 0; i < easterEggs.length; i++) {
+        for (var i = 0; i < easterEggs.length; i++) {
             var eggLength = easterEggs[i].length;
             match = keyHistory.match(easterEggs[i]);
             if (match) {
                 //alert(match);
                 switch(match.toString()){
                     case 'reset':
-                        stopPlay();
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         reset();
                         break;
                     case 'fuchsia':
-                        stopPlay();
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         effect(2);
                         break;
                     case 'harley':
@@ -31,28 +38,64 @@ function eggs() {
                         loadUrl("https://www.upwork.com/freelancers/~016cd3dcf007c09abd");
                         break;
                     case 'jedi':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/ut4WoDi-AEM?rel=0&amp;start=11");
                         break;
                     case 'anjuna':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/jiLkBxw2pbs?rel=0");
                         break;
                     case 'asot':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/pv4xBTrNeoQ?rel=0");
                         break;
                     case 'progressive':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/KvRVky0r7YM?rel=0");
                         break;
                     case 'chill':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/nUlazURAjDE?rel=0");
                         break;
                     case 'jazz':
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         play("https://www.youtube-nocookie.com/embed/Vls4h1GAP-c?rel=0");
                         break;
                     default:
-                        stopPlay();
+                        if(winter) {
+                            stopPlay(true);
+                            winter = false;
+                        }
                         effect(1);
                         var url = "/assets/images/backgrounds/" + match + ".jpg";
                         $("body").css("background", "url(" + url + ") 0px 65px");
+                        if(match.toString()==='winter' && winter === false){
+                            winter = true;
+                            stopPlay(false);
+                            $.getScript("/assets/js/snowstorm-min.js", function(){
+                                snowStorm.flakesMaxActive = 75;
+                                snowStorm.useTwinkleEffect = true;
+                                snowStorm.toggleSnow();
+                            });
+                        }
                 }
                 keyHistory = match = '';
             } else if (keyHistory.length > 30) {
@@ -62,13 +105,13 @@ function eggs() {
     });
 }
 
-function stopPlay(){
+function stopPlay(n){
+    if(n===true){snowStorm.toggleSnow();}
     $("#vid").remove();
     $("#youtube").css({"height":"0", "visibility":"hidden"});
 }
 
 function play(url){
-    stopPlay();
     effect(1);
     $("#youtube").css({"height":"315", "visibility":"visible"});
     $("#youtube").append('<iframe id="vid" width="560" height="315" src="" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
@@ -82,7 +125,7 @@ function reset(){
 }
 
 function loadUrl(url){
-    stopPlay();
+    stopPlay(false);
     window.open(url, "_self");
 }
 
@@ -95,9 +138,11 @@ function effect(n){
     if(n===1){
         $("body").css({"background-color":"black","height":"1145","background-image":"none"});
         $(".site-header").css({"border-bottom":"5px solid #424242","border-top":"5px solid #424242"});
+        $("nav").css({"visibility":"visible"});
     } else {
         $("body").css({"background-color":"#FF0080","height":"1080","background-image":"none"});
         $(".site-header").css({"border-bottom":"5px solid #FF0080","border-top":"5px solid #FF0080"});
+        $("nav").css({"visibility":"hidden"});
     }
     window.scrollTo(0,0);
 }
