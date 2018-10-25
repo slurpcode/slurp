12b741c76d32cd0f963c4d96b3ec34c3106034de
John Bampton <jbampton@users.noreply.github.com>
2018-10-25 12:23:49 +1000

    Add Hadouken fireball horizontal

--- assets/js/eggs.js
+++ assets/js/eggs.js
@@ -17,7 +17,7 @@ function eggs() {
     $("#overlay").click(function () {
         reset();
     });
-    var easterEggs = ['reset', 'fuchsia', 'king', 'winter', 'digger', 'matrix', 'jurassic', 'christmas', 'easter', 'flowers', 'orange', 'yeti', 'min', 'alien', 'dino', 'bear', 'bubble', 'ghost', 'vader', 'thexder', 'harley', 'jorge', 'barney', 'karine', 'jedi', 'anjuna', 'asot', 'progressive', 'chill', 'jazz'];
+    var easterEggs = ['reset', 'fuchsia', 'king', 'winter', 'digger', 'matrix', 'jurassic', 'christmas', 'easter', 'flowers', 'orange', 'yeti', 'min', 'alien', 'dino', 'bear', 'bubble', 'ghost', 'vader', 'thexder', 'fireball', 'harley', 'jorge', 'barney', 'karine', 'jedi', 'anjuna', 'asot', 'progressive', 'chill', 'jazz'];
     var keyHistory = '';
     var match;
     var winter = false;
@@ -109,7 +109,7 @@ function eggs() {
                         }
                         effect(1);
                         var background = match.toString();
-                        if(background==='orange' || background==='yeti' || background==='min' || background==='alien' || background==='dino' || background==='bear' || background==='bubble' || background==='ghost' || background==='vader' || background==='thexder' || background==='flowers'){
+                        if(background==='orange' || background==='yeti' || background==='min' || background==='alien' || background==='dino' || background==='bear' || background==='bubble' || background==='ghost' || background==='vader' || background==='thexder' || background==='fireball' || background==='flowers'){
                             background = 'flowers';
                         }
                         var url = "/assets/images/backgrounds/" + background + ".jpg";
@@ -128,6 +128,11 @@ function eggs() {
                         if(match.toString()==='orange' || match.toString()==='yeti' || match.toString()==='min' || match.toString()==='alien' || match.toString()==='dino' || match.toString()==='bear' || match.toString()==='bubble' || match.toString()==='ghost' || match.toString()==='vader' || match.toString()==='thexder'){
                             $("#youtube").after("<canvas id=\"c\"></canvas>");
                             beasts(match.toString());
+                        }
+
+                        if(match.toString()==='fireball'){
+                            $("#youtube").after("<canvas id=\"c\"></canvas>");
+                            beastsHorizontal(match.toString());
                         }
 
                         if(match.toString()==='winter' && winter === false){
@@ -191,6 +196,55 @@ function beasts(background){
                 drops[i] = 0;
 
             //incrementing Y coordinate
+            drops[i] +=  Math.floor(Math.random()*1.5);
+        }
+    }
+    setInterval(draw, 33);
+}
+
+function beastsHorizontal(background){
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    c.height = document.body.scrollHeight;
+    c.width = window.innerWidth;
+    var font_size = 32;
+    var rows = c.height/font_size;
+    $("#source").attr("src", "/assets/images/beasts/" + background + ".png");
+    var image = document.getElementById('source');
+    //an array of drops - one per row
+    var drops = [];
+
+
+//j = x co-ordinate of the drop(same for every drop initially at x=0 moving left to right
+    for(var j = 0; j < rows; j++)
+        drops[j] = 0;
+
+//drawing the characters
+    function draw()
+    {
+        //Black BG for the canvas
+        //translucent BG to show trail
+        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+        ctx.fillRect(0, 0, c.width, c.height);
+
+        ctx.fillStyle = "blue"; //orange text
+        ctx.font = font_size + "px arial";
+        //looping over drops
+        for(var i = 0; i < drops.length; i++)
+        {
+            //a random chinese character to print
+            //var text = chinese[Math.floor(Math.random()*chinese.length)];
+            //x = i*font_size, y = value of drops[i]*font_size
+            //ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+            ctx.drawImage(image, drops[i]*font_size, i*font_size);
+
+            //sending the drop back to the top randomly after it has crossed the screen
+            //adding a randomness to the reset to make the drops scattered on the Y axis
+            if(drops[i]*font_size > c.width && Math.random() > 0.975)
+                drops[i] = 0;
+
+            //incrementing X coordinate
             drops[i] +=  Math.floor(Math.random()*1.5);
         }
     }
