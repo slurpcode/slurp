9d524032ab878c5680208d930b7b670850dca8a7
John Bampton <jbampton@gmail.com>
2019-01-29 03:06:59 +1000

    Cold weather

--- assets/js/start.js
+++ assets/js/start.js
@@ -12,5 +12,36 @@ $(document).ready(function() {
         document.title = title;
         index++;
     }, 1000);
+    findWeatherDetails();
     eggs();
 });
+function findWeatherDetails() {
+    const appid = '60a010ca788e032f55597166c459f0cf';
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=Tromsø,Norway&appid=" + appid;
+    httpRequestAsync(searchLink, theResponse);
+}
+
+function theResponse(response) {
+    let cityName = document.getElementById("city-name");
+    let icon = document.getElementById("icon");
+    let temperature = document.getElementById("temp");
+    let humidity = document.getElementById("humidity-div");
+
+    let jsonObject = JSON.parse(response);
+    cityName.innerHTML = jsonObject.name;
+    icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+    temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+    humidity.innerHTML = jsonObject.main.humidity + "%";
+}
+
+function httpRequestAsync(url, callback)
+{
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous
+    httpRequest.send();
+}
+
