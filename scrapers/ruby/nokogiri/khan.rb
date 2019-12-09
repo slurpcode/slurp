9041bcdf3ab89f808d789539cbfa252ed5e33123
Restyled.io <commits@restyled.io>
2019-12-09 14:38:46 +0000

    Restyled by prettier-ruby

--- scrapers/ruby/nokogiri/khan.rb
+++ scrapers/ruby/nokogiri/khan.rb
@@ -5,9 +5,11 @@ require 'open-uri'
 
 def get_energy_points(user)
   uri = URI.parse("https://www.khanacademy.org/profile/#{user}/")
-  doc = Nokogiri::HTML(uri.open)
-  res = doc.xpath('//script[contains(.,"prefersReducedMotion")]')
-           .to_s.scan(/points":(\d+),"prefersReducedMotion/)
+  doc = Nokogiri.HTML(uri.open)
+  res =
+    doc.xpath('//script[contains(.,"prefersReducedMotion")]').to_s.scan(
+      /points":(\d+),"prefersReducedMotion/
+    )
   res.size.positive? ? res : 'Not found'
 end
 
