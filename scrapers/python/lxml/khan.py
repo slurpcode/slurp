f5a6f94678446fc46160e7ecab78dc478ce043f4
Restyled.io <commits@restyled.io>
2019-12-09 14:38:42 +0000

    Restyled by black

--- scrapers/python/lxml/khan.py
+++ scrapers/python/lxml/khan.py
@@ -7,16 +7,17 @@ from lxml import html  # pylint:disable=import-error
 
 def get_energy_points(user):
     """Return the wonderful energy points."""
-    page = requests.get('https://www.khanacademy.org/profile/%s/' % user)
+    page = requests.get("https://www.khanacademy.org/profile/%s/" % user)
     tree = html.fromstring(page.content)
     text = tree.xpath('//script[contains(.,"prefersReducedMotion")]/text()')
-    points = 'Not found'
+    points = "Not found"
     for t in text:  # pylint:disable=invalid-name
-        m = re.search(r'points":(\d+),"prefersReducedMotion',
-                      t)  # pylint:disable=invalid-name
+        m = re.search(
+            r'points":(\d+),"prefersReducedMotion', t
+        )  # pylint:disable=invalid-name
         if m:
             points = m.group(1)
     return points
 
 
-print(get_energy_points('XSLTGod'))  # pylint:disable=superfluous-parens
+print(get_energy_points("XSLTGod"))  # pylint:disable=superfluous-parens
