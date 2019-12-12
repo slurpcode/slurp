c6a06af302c9e1d0a3856d0f9db9bdd82cc95c99
lastch <elicloudh@gmail.com>
2019-12-12 11:00:39 +0200

    update

--- scrapers/python/logo-downloader.py
+++ scrapers/python/logo-downloader.py
@@ -4,17 +4,16 @@ import requests
 import time
 
 mypath = 'logos'
-img_url = 'https://raw.githubusercontent.com/fuchsia-programming/fuchsia-programming.github.io/master/assets/images/logos/'
+img_url = 'https://raw.githubusercontent.com/fuchsia-programming \
+/fuchsia-programming.github.io/master/assets/images/logos/'
 
 if not os.path.isdir(mypath):
    os.makedirs(mypath)
-
 
 def check_arguments():
     if len(sys.argv) > 1:
         return sys.argv[1]
     return False
-
 
 def working_directory():
     owd = os.getcwd()
@@ -27,21 +26,16 @@ def working_directory():
             j = 306
         else:
             j = number
-
         for i in range(int(j)+1):
             img_name = 'fuchsia-'+str(i)+'.png'
             with open(img_name, 'wb') as handle:
                 response = requests.get(img_url+img_name, stream=True)
-
                 if not response.ok:
                     print(response)
-
                 for block in response.iter_content(1024):
                     if not block:
                         break
-
                     handle.write(block)
-
             time.sleep(5)
     else:
         sys.exit()
@@ -49,8 +43,6 @@ def working_directory():
 def main():
     cur_path = working_directory()
     print(cur_path)
-
-
 
 if __name__ == "__main__":
     main()
