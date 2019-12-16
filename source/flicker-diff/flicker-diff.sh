0a3f7db1aa2a5095518b5a86dfdccf240443c1f7
Restyled.io <commits@restyled.io>
2019-12-16 00:26:53 +0000

    Restyled by shfmt

--- source/flicker-diff/flicker-diff.sh
+++ source/flicker-diff/flicker-diff.sh
@@ -10,7 +10,7 @@
 # table of contents
 # valid HTML5
 # other view options like side by side or overlay markers
-# command line args 
+# command line args
 
 #fullfilename="$1"
 #filename=${fullfilename%%.*}
@@ -30,24 +30,23 @@ pdftocairo -png old/"$filename".pdf old/images/"$filename"/"$filename"
 pdftocairo -png new/"$filename".pdf new/images/"$filename"/"$filename"
 
 # Fix to get number or pages ???
-for i in {01..29};
-do
-# create the difference between each png page and generate a flicker based GIF image difference
-    convert -delay 50 old/images/"$filename"/"$filename"-"$i".png new/images/"$filename"/"$filename"-"$i".png flicker/"$filename"/"$filename"-"$i"-difference-flicker.gif
+for i in {01..29}; do
+  # create the difference between each png page and generate a flicker based GIF image difference
+  convert -delay 50 old/images/"$filename"/"$filename"-"$i".png new/images/"$filename"/"$filename"-"$i".png flicker/"$filename"/"$filename"-"$i"-difference-flicker.gif
 done
 
 # zero file
-:> flicker-diff-webpage.html
+: >flicker-diff-webpage.html
 
 # Open file descriptor (fd) 3 for read/write on a HTML file.
-exec 3<> flicker-diff-webpage.html
+exec 3<>flicker-diff-webpage.html
 
 # create web page header
 echo "<!doctype html><html><head><meta charset=\"UTF-8\"></head><body>" >&3
 
 # loop over flicker GIFs
 for f in flicker/"$filename"/*; do
-    echo "<img src='$f'>" >&3
+  echo "<img src='$f'>" >&3
 done
 # finish HTML page
 echo "</body></html>" >&3
