54e77f135dbe25a94b613f6f4cace924da401a84
Restyled.io <commits@restyled.io>
2020-01-08 02:14:36 +0000

    Restyled by prettier

--- docs/rdoc/js/darkfish.js
+++ docs/rdoc/js/darkfish.js
@@ -21,66 +21,68 @@ if (!("console" in window) || !("firebug" in console)) {
 */
 
 function showSource(e) {
-  var target = e.target;
-  while (!target.classList.contains('method-detail')) {
-    target = target.parentNode;
-  }
-  if (typeof target !== "undefined" && target !== null) {
-    target = target.querySelector('.method-source-code');
-  }
-  if (typeof target !== "undefined" && target !== null) {
-    target.classList.toggle('active-menu')
-  }
-};
+    var target = e.target;
+    while (!target.classList.contains("method-detail")) {
+        target = target.parentNode;
+    }
+    if (typeof target !== "undefined" && target !== null) {
+        target = target.querySelector(".method-source-code");
+    }
+    if (typeof target !== "undefined" && target !== null) {
+        target.classList.toggle("active-menu");
+    }
+}
 
 function hookSourceViews() {
-  document.querySelectorAll('.method-heading').forEach(function(codeObject) {
-    codeObject.addEventListener('click', showSource);
-  });
-};
+    document.querySelectorAll(".method-heading").forEach(function(codeObject) {
+        codeObject.addEventListener("click", showSource);
+    });
+}
 
 function hookSearch() {
-  var input = document.querySelector('#search-field');
-  var result = document.querySelector('#search-results');
-  result.classList.remove("initially-hidden");
+    var input = document.querySelector("#search-field");
+    var result = document.querySelector("#search-results");
+    result.classList.remove("initially-hidden");
 
-  var search_section = document.querySelector('#search-section');
-  search_section.classList.remove("initially-hidden");
+    var search_section = document.querySelector("#search-section");
+    search_section.classList.remove("initially-hidden");
 
-  var search = new Search(search_data, input, result);
+    var search = new Search(search_data, input, result);
 
-  search.renderItem =
-      function(result) {
-    var li = document.createElement('li');
-    var html = '';
+    search.renderItem = function(result) {
+        var li = document.createElement("li");
+        var html = "";
 
-    // TODO add relative path to <script> per-page
-    html += '<p class="search-match"><a href="' + index_rel_prefix +
-            result.path + '">' + this.hlt(result.title);
-    if (result.params)
-      html += '<span class="params">' + result.params + '</span>';
-    html += '</a>';
+        // TODO add relative path to <script> per-page
+        html +=
+            '<p class="search-match"><a href="' +
+            index_rel_prefix +
+            result.path +
+            '">' +
+            this.hlt(result.title);
+        if (result.params)
+            html += '<span class="params">' + result.params + "</span>";
+        html += "</a>";
 
-    if (result.namespace)
-      html += '<p class="search-namespace">' + this.hlt(result.namespace);
+        if (result.namespace)
+            html += '<p class="search-namespace">' + this.hlt(result.namespace);
 
-    if (result.snippet)
-      html += '<div class="search-snippet">' + result.snippet + '</div>';
+        if (result.snippet)
+            html += '<div class="search-snippet">' + result.snippet + "</div>";
 
-    li.innerHTML = html;
+        li.innerHTML = html;
 
-    return li;
-  }
+        return li;
+    };
 
-      search.select =
-          function(result) {
-    window.location.href = result.firstChild.firstChild.href;
-  }
+    search.select = function(result) {
+        window.location.href = result.firstChild.firstChild.href;
+    };
 
-          search.scrollIntoView = search.scrollInWindow;
-};
+    search.scrollIntoView = search.scrollInWindow;
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-  hookSourceViews();
-  hookSearch();
+document.addEventListener("DOMContentLoaded", function() {
+    hookSourceViews();
+    hookSearch();
 });
