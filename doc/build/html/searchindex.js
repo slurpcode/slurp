48d3593dd74ca6a382d26d41bbf7da2da754eaba
Restyled.io <commits@restyled.io>
2020-01-09 20:03:28 +0000

    Restyled by prettier

--- doc/build/html/searchindex.js
+++ doc/build/html/searchindex.js
@@ -1,22 +1,22 @@
 Search.setIndex({
-  docnames : [ "index" ],
-  envversion : {
-    "sphinx.domains.c" : 1,
-    "sphinx.domains.changeset" : 1,
-    "sphinx.domains.citation" : 1,
-    "sphinx.domains.cpp" : 1,
-    "sphinx.domains.javascript" : 1,
-    "sphinx.domains.math" : 2,
-    "sphinx.domains.python" : 1,
-    "sphinx.domains.rst" : 1,
-    "sphinx.domains.std" : 1,
-    sphinx : 56
-  },
-  filenames : [ "index.rst" ],
-  objects : {},
-  objnames : {},
-  objtypes : {},
-  terms : {index : 0, modul : 0, page : 0, search : 0},
-  titles : [ "Welcome to slurp\u2019s documentation!" ],
-  titleterms : {document : 0, indic : 0, slurp : 0, tabl : 0, welcom : 0}
-})
+    docnames: ["index"],
+    envversion: {
+        "sphinx.domains.c": 1,
+        "sphinx.domains.changeset": 1,
+        "sphinx.domains.citation": 1,
+        "sphinx.domains.cpp": 1,
+        "sphinx.domains.javascript": 1,
+        "sphinx.domains.math": 2,
+        "sphinx.domains.python": 1,
+        "sphinx.domains.rst": 1,
+        "sphinx.domains.std": 1,
+        sphinx: 56
+    },
+    filenames: ["index.rst"],
+    objects: {},
+    objnames: {},
+    objtypes: {},
+    terms: { index: 0, modul: 0, page: 0, search: 0 },
+    titles: ["Welcome to slurp\u2019s documentation!"],
+    titleterms: { document: 0, indic: 0, slurp: 0, tabl: 0, welcom: 0 }
+});
