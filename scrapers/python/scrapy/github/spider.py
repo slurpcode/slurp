0954c0ca1a0db072c43ee2d15506f54d008adb69
Restyled.io <commits@restyled.io>
2020-02-29 18:09:57 +0000

    Restyled by black

--- scrapers/python/scrapy/github/spider.py
+++ scrapers/python/scrapy/github/spider.py
@@ -5,14 +5,15 @@ import scrapy
 
 class GithubSpider(scrapy.Spider):
     """Lets crawl."""
-    name = 'githubspider'
-    start_urls = ['https://github.com/jbampton']
+
+    name = "githubspider"
+    start_urls = ["https://github.com/jbampton"]
 
     def parse(self, response):
         """Grab the 5 stats."""
         output = {}
         for repos in response.xpath("//a[contains(@href,'tab=repositories')]/span"):
-            output['repositories'] = repos.css('::text').get().strip()
+            output["repositories"] = repos.css("::text").get().strip()
         for stars in response.xpath("//a[contains(@href,'tab=stars')]/span"):
-            output['stars'] = stars.css('::text').get().strip()
+            output["stars"] = stars.css("::text").get().strip()
         yield output
