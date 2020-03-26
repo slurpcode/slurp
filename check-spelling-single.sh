6615b3ae8ff9f85cc0cb3502e05a3766e392f2b5
Restyled.io <commits@restyled.io>
2020-03-26 09:41:26 +0000

    Restyled by shfmt

--- check-spelling-single.sh
+++ check-spelling-single.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-while read -r file
-do
-    aspell list --lang=en --encoding=utf-8 --personal=./.aspell.en.pws < "$file" | sort -u
-done < <(find . -name "*.md" -not -path "**/node_modules/*" -not -path "**/charts/*");
+while read -r file; do
+  aspell list --lang=en --encoding=utf-8 --personal=./.aspell.en.pws <"$file" | sort -u
+done < <(find . -name "*.md" -not -path "**/node_modules/*" -not -path "**/charts/*")
