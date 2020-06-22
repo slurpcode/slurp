c53435a82fc1026876bb9e7160dd8d1508a18a6d
Restyled.io <commits@restyled.io>
2020-06-22 15:55:33 +0000

    Restyled by black

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
-    'navigation_depth': 4,
+    "navigation_depth": 4,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -50,5 +50,5 @@ html_static_path = ["_static"]
 
 html_logo = "_static/images/logo.jpeg"
 
-locale_dirs = ['locale/']   # path is example but recommended.
-gettext_compact = False     # optional.
+locale_dirs = ["locale/"]  # path is example but recommended.
+gettext_compact = False  # optional.
