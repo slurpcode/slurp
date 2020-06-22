a73cd05f7b9e1d74f2a7dc19359c212131549100
John Bampton <jbampton@users.noreply.github.com>
2020-06-23 05:46:11 +1000

    Remove the label action since it doesn't work on forks.

--- lint.sh
+++ lint.sh
@@ -5,4 +5,4 @@ find . -name "*.py" -not -path "**/venv/*" | xargs pylint --disable=C0103,C0114
 mdl .
 rubocop
 reek -c .reek.yml
-yamllint .erb-lint.yml .hound.yml .overcommit.yml .percy.yml .reek.yml .rubocop.yml .travis.yml .circleci/config.yml .github/labeler.yml .github/workflows/greetings.yml .github/workflows/label.yml
+yamllint .erb-lint.yml .hound.yml .overcommit.yml .percy.yml .reek.yml .rubocop.yml .travis.yml .circleci/config.yml .github/workflows/greetings.yml
