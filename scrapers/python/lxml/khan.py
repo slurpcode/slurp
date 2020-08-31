43ef1e91087f13809c368d63c16b35801233d65d
Restyled.io <commits@restyled.io>
2020-08-31 16:24:51 +0000

    Restyled by black

--- scrapers/python/lxml/khan.py
+++ scrapers/python/lxml/khan.py
@@ -5,11 +5,12 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
 
-browser = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
+browser = webdriver.Chrome("./chromedriver.exe", chrome_options=options)
 
 browser.get("https://www.khanacademy.org/profile/JohnBampton/")
 
 time.sleep(35)
 energy_points = browser.find_element_by_xpath(
-    '/html/body/div[2]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/div/div[4]/div/div/div/div[1]/div[1]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]/span')
+    "/html/body/div[2]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/div/div[4]/div/div/div/div[1]/div[1]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]/span"
+)
 print(int(energy_points.text.replace(",", "")))
