ca02470dd325e8f5eb17100f34fcec70179797b7
Restyled.io <commits@restyled.io>
2020-08-31 16:24:49 +0000

    Restyled by autopep8

--- scrapers/python/lxml/khan.py
+++ scrapers/python/lxml/khan.py
@@ -10,5 +10,6 @@ browser = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
 browser.get("https://www.khanacademy.org/profile/JohnBampton/")
 
 time.sleep(35)
-energy_points = browser.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/div/div[4]/div/div/div/div[1]/div[1]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]/span')
+energy_points = browser.find_element_by_xpath(
+    '/html/body/div[2]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/div/div[4]/div/div/div/div[1]/div[1]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]/span')
 print(int(energy_points.text.replace(",", "")))
