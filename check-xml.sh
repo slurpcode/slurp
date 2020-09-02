5d06ca3e0420c4e1de36e46b63dd75faaa385d18
shravanshetty1 <shravanshetty322@gmail.com>
2020-09-03 00:01:44 +0530

    232 - resolving shell check issues

--- check-xml.sh
+++ check-xml.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # print first
+# shellcheck disable=SC2016
 find . -type f -not -path "./docs/*" -not -path "./logorama/*" -not -path "./vendor/*" -name '*.xml' -printf '%p\n' | xargs -n1 sh -c 'xmllint $0 > /dev/null'
 
 # exit if invalid
