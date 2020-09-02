c906a4f96c976bd92122d1a90c5ca32af759d717
shravanshetty1 <shravanshetty322@gmail.com>
2020-09-02 23:38:17 +0530

    excluding vendor directory

--- check-xml.sh
+++ check-xml.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # print first
-find . -type f -not -path "./docs/*" -not -path "./logorama/*" -name '*.xml' -printf '%p\n' | xargs -n1 sh -c 'xmllint $0 > /dev/null'
+find . -type f -not -path "./docs/*" -not -path "./logorama/*" -not -path "./vendor/*" -name '*.xml' -printf '%p\n' | xargs -n1 sh -c 'xmllint $0 > /dev/null'
 
 # exit if invalid
 # shellcheck disable=SC2016
-find . -type f -not -path "./docs/*" -not -path "./logorama/*" -name '*.xml' -printf '%p\n' | xargs -n1 sh -c 'xmllint $0 || exit 255' >/dev/null
+find . -type f -not -path "./docs/*" -not -path "./logorama/*" -not -path "./vendor/*" -name '*.xml' -printf '%p\n' | xargs -n1 sh -c 'xmllint $0 || exit 255' >/dev/null
