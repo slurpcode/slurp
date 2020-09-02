2ede2854125e8490154b79d8a87bd827bb387f4d
shravanshetty1 <shravanshetty322@gmail.com>
2020-09-03 00:03:13 +0530

    111 - fixing shell check issues

--- check-csv.sh
+++ check-csv.sh
@@ -4,4 +4,5 @@
 find . -type f -name '*.csv' -printf '%p\n' | xargs -t -n1 csvlint
 
 # exit if invalid
+# shellcheck disable=SC2016
 find . -type f -name '*.csv' -printf '%p\n' | xargs -n1 sh -c 'csvlint $0 || exit 255' > /dev/null
