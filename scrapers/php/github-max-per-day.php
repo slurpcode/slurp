632f22d8a4beb4ded74d80cf54516d3d016f8dd2
Restyled.io <commits@restyled.io>
2021-05-16 14:54:59 +0000

    Restyled by php-cs-fixer

--- scrapers/php/github-max-per-day.php
+++ scrapers/php/github-max-per-day.php
@@ -10,7 +10,7 @@ $content = get_content($url);
 $matches = [];
 preg_match_all('/data-count="(.*?)"/m', $content, $matches);
 $result = '';
-if(!empty($matches[1]) && is_array($matches[1])) {
+if (!empty($matches[1]) && is_array($matches[1])) {
     rsort($matches[1]);
     $result = array_shift($matches[1]);
 }
