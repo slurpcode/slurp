aa47718152d340f9d23efa521f246b786b0b190c
Max Base <MaxBaseCode@Gmail.Com>
2021-05-20 10:59:50 +0430

    Improve ident and remove new line before "{" (similar to github-max-per-day.php file)

--- scrapers/php/khan.php
+++ scrapers/php/khan.php
@@ -1,13 +1,11 @@
 <?php
+include('base/common.php');
 
-    include('base/common.php');
+function get_energy_points($user) {
+    $url = "https://www.khanacademy.org/profile/$user/";
+    $content = get_content($url);
+    preg_match('/points":(\\d+),"prefersReducedMotion/', $content, $matches);
+    return ($matches[1] ? $matches[1] : 'Not found').PHP_EOL;
+}
 
-    function get_energy_points($user)
-    {
-        $url = "https://www.khanacademy.org/profile/$user/";
-        $content = get_content($url);
-        preg_match('/points":(\\d+),"prefersReducedMotion/', $content, $matches);
-        return ($matches[1] ? $matches[1] : 'Not found').PHP_EOL;
-    }
-
-    echo get_energy_points('XSLTGod');
+echo get_energy_points('XSLTGod'); // Put username...
