0fd3f2e4a17da81f0404d8c08526053f9ab2677c
Max Base <MaxBaseCode@Gmail.Com>
2021-05-20 11:13:53 +0430

    khan.php file: Change function name

--- scrapers/php/khan.php
+++ scrapers/php/khan.php
@@ -1,11 +1,12 @@
 <?php
 require 'base/common.php';
 
-function get_energy_points($user) {
+function Get_Energy_points($user)
+{
     $url = "https://www.khanacademy.org/profile/$user/";
     $content = get_content($url);
     preg_match('/points":(\\d+),"prefersReducedMotion/', $content, $matches);
     return ($matches[1] ? $matches[1] : 'Not found').PHP_EOL;
 }
 
-echo get_energy_points('XSLTGod'); // Put username...
+echo Get_Energy_points('XSLTGod'); // Put username...
