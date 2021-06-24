2292bdb189a3fa68439cd00a7cb45e831f33719e
Restyled.io <commits@restyled.io>
2021-06-24 09:24:06 +0000

    Restyled by shfmt

--- pdfstar/tools/check-revive.sh
+++ pdfstar/tools/check-revive.sh
@@ -7,9 +7,8 @@ revive -config config.toml .
 
 var=$(revive -config config.toml .)
 # then exit with fail if found
-if test -z "$var"
-then
-    exit 0
+if test -z "$var"; then
+  exit 0
 else
-    exit 1
+  exit 1
 fi
