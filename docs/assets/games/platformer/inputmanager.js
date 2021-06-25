2e1369f2068143ce7a9ac7adf53198c698a97b5e
Restyled.io <commits@restyled.io>
2021-06-25 13:34:44 +0000

    Restyled by prettier

--- docs/assets/games/platformer/inputmanager.js
+++ docs/assets/games/platformer/inputmanager.js
@@ -16,22 +16,20 @@
  * @memberof gdjs
  * @class InputManager
  */
-gdjs.InputManager = function() {
-  this._pressedKeys = new Hashtable();
-  this._releasedKeys = new Hashtable();
-  this._lastPressedKey = 0;
-  this._pressedMouseButtons = new Array(5);
-  this._releasedMouseButtons = new Array(5);
-  this._mouseX = 0;
-  this._mouseY = 0;
-  this._mouseWheelDelta = 0;
-  this._touches = new Hashtable();
-  this._startedTouches =
-      []; // Identifiers of the touches that started during/before the frame.
-  this._endedTouches =
-      []; // Identifiers of the touches that ended during/before the frame.
+gdjs.InputManager = function () {
+    this._pressedKeys = new Hashtable();
+    this._releasedKeys = new Hashtable();
+    this._lastPressedKey = 0;
+    this._pressedMouseButtons = new Array(5);
+    this._releasedMouseButtons = new Array(5);
+    this._mouseX = 0;
+    this._mouseY = 0;
+    this._mouseWheelDelta = 0;
+    this._touches = new Hashtable();
+    this._startedTouches = []; // Identifiers of the touches that started during/before the frame.
+    this._endedTouches = []; // Identifiers of the touches that ended during/before the frame.
 
-  this._touchSimulateMouse = true;
+    this._touchSimulateMouse = true;
 };
 
 /** @constant {number} */
@@ -47,34 +45,36 @@ gdjs.InputManager.MOUSE_MIDDLE_BUTTON = 2;
  * Should be called whenever a key is pressed
  * @param {number} keyCode The key code associated to the key press.
  */
-gdjs.InputManager.prototype.onKeyPressed = function(keyCode) {
-  this._pressedKeys.put(keyCode, true);
-  this._lastPressedKey = keyCode;
+gdjs.InputManager.prototype.onKeyPressed = function (keyCode) {
+    this._pressedKeys.put(keyCode, true);
+    this._lastPressedKey = keyCode;
 };
 
 /**
  * Should be called whenever a key is released
  * @param {number} keyCode The key code associated to the key release.
  */
-gdjs.InputManager.prototype.onKeyReleased = function(keyCode) {
-  this._pressedKeys.put(keyCode, false);
-  this._releasedKeys.put(keyCode, true);
+gdjs.InputManager.prototype.onKeyReleased = function (keyCode) {
+    this._pressedKeys.put(keyCode, false);
+    this._releasedKeys.put(keyCode, true);
 };
 
 /**
  * Return the code of the last key that was pressed.
  * @return {number} The code of the last key pressed.
  */
-gdjs.InputManager.prototype.getLastPressedKey =
-    function() { return this._lastPressedKey; };
+gdjs.InputManager.prototype.getLastPressedKey = function () {
+    return this._lastPressedKey;
+};
 
 /**
  * Return true if the key corresponding to keyCode is pressed.
  * @param {number} keyCode The key code to be tested.
  */
-gdjs.InputManager.prototype.isKeyPressed = function(keyCode) {
-  return this._pressedKeys.containsKey(keyCode) &&
-         this._pressedKeys.get(keyCode);
+gdjs.InputManager.prototype.isKeyPressed = function (keyCode) {
+    return (
+        this._pressedKeys.containsKey(keyCode) && this._pressedKeys.get(keyCode)
+    );
 };
 
 /**
@@ -82,24 +82,26 @@ gdjs.InputManager.prototype.isKeyPressed = function(keyCode) {
  * frame.
  * @param {number} keyCode The key code to be tested.
  */
-gdjs.InputManager.prototype.wasKeyReleased = function(keyCode) {
-  return this._releasedKeys.containsKey(keyCode) &&
-         this._releasedKeys.get(keyCode);
+gdjs.InputManager.prototype.wasKeyReleased = function (keyCode) {
+    return (
+        this._releasedKeys.containsKey(keyCode) &&
+        this._releasedKeys.get(keyCode)
+    );
 };
 
 /**
  * Return true if any key is pressed
  */
-gdjs.InputManager.prototype.anyKeyPressed = function() {
-  for (var keyCode in this._pressedKeys.items) {
-    if (this._pressedKeys.items.hasOwnProperty(keyCode)) {
-      if (this._pressedKeys.items[keyCode]) {
-        return true;
-      }
+gdjs.InputManager.prototype.anyKeyPressed = function () {
+    for (var keyCode in this._pressedKeys.items) {
+        if (this._pressedKeys.items.hasOwnProperty(keyCode)) {
+            if (this._pressedKeys.items[keyCode]) {
+                return true;
+            }
+        }
     }
-  }
 
-  return false;
+    return false;
 };
 
 /**
@@ -110,9 +112,9 @@ gdjs.InputManager.prototype.anyKeyPressed = function() {
  * @param {number} x The mouse new X position
  * @param {number} y The mouse new Y position
  */
-gdjs.InputManager.prototype.onMouseMove = function(x, y) {
-  this._mouseX = x;
-  this._mouseY = y;
+gdjs.InputManager.prototype.onMouseMove = function (x, y) {
+    this._mouseX = x;
+    this._mouseY = y;
 };
 
 /**
@@ -120,14 +122,18 @@ gdjs.InputManager.prototype.onMouseMove = function(x, y) {
  *
  * @return the mouse X position, relative to the game view.
  */
-gdjs.InputManager.prototype.getMouseX = function() { return this._mouseX; };
+gdjs.InputManager.prototype.getMouseX = function () {
+    return this._mouseX;
+};
 
 /**
  * Get the mouse Y position
  *
  * @return the mouse Y position, relative to the game view.
  */
-gdjs.InputManager.prototype.getMouseY = function() { return this._mouseY; };
+gdjs.InputManager.prototype.getMouseY = function () {
+    return this._mouseY;
+};
 
 /**
  * Should be called whenever a mouse button is pressed
@@ -135,9 +141,9 @@ gdjs.InputManager.prototype.getMouseY = function() { return this._mouseY; };
  * See gdjs.InputManager.MOUSE_LEFT_BUTTON,
  * gdjs.InputManager.MOUSE_RIGHT_BUTTON, gdjs.InputManager.MOUSE_MIDDLE_BUTTON
  */
-gdjs.InputManager.prototype.onMouseButtonPressed = function(buttonCode) {
-  this._pressedMouseButtons[buttonCode] = true;
-  this._releasedMouseButtons[buttonCode] = false;
+gdjs.InputManager.prototype.onMouseButtonPressed = function (buttonCode) {
+    this._pressedMouseButtons[buttonCode] = true;
+    this._releasedMouseButtons[buttonCode] = false;
 };
 
 /**
@@ -145,9 +151,9 @@ gdjs.InputManager.prototype.onMouseButtonPressed = function(buttonCode) {
  * @param {number} buttonCode The mouse button code associated to the event.
  *     (see onMouseButtonPressed)
  */
-gdjs.InputManager.prototype.onMouseButtonReleased = function(buttonCode) {
-  this._pressedMouseButtons[buttonCode] = false;
-  this._releasedMouseButtons[buttonCode] = true;
+gdjs.InputManager.prototype.onMouseButtonReleased = function (buttonCode) {
+    this._pressedMouseButtons[buttonCode] = false;
+    this._releasedMouseButtons[buttonCode] = true;
 };
 
 /**
@@ -155,9 +161,11 @@ gdjs.InputManager.prototype.onMouseButtonReleased = function(buttonCode) {
  * @param {number} buttonCode The mouse button code (0: Left button, 1: Right
  *     button).
  */
-gdjs.InputManager.prototype.isMouseButtonPressed = function(buttonCode) {
-  return this._pressedMouseButtons[buttonCode] !== undefined &&
-         this._pressedMouseButtons[buttonCode];
+gdjs.InputManager.prototype.isMouseButtonPressed = function (buttonCode) {
+    return (
+        this._pressedMouseButtons[buttonCode] !== undefined &&
+        this._pressedMouseButtons[buttonCode]
+    );
 };
 
 /**
@@ -166,34 +174,37 @@ gdjs.InputManager.prototype.isMouseButtonPressed = function(buttonCode) {
  * @param {number} buttonCode The mouse button code (0: Left button, 1: Right
  *     button).
  */
-gdjs.InputManager.prototype.isMouseButtonReleased = function(buttonCode) {
-  return this._releasedMouseButtons[buttonCode] !== undefined &&
-         this._releasedMouseButtons[buttonCode];
+gdjs.InputManager.prototype.isMouseButtonReleased = function (buttonCode) {
+    return (
+        this._releasedMouseButtons[buttonCode] !== undefined &&
+        this._releasedMouseButtons[buttonCode]
+    );
 };
 
 /**
  * Should be called whenever the mouse wheel is used
  * @param {number} wheelDelta The mouse wheel delta
  */
-gdjs.InputManager.prototype.onMouseWheel = function(
-    wheelDelta) { this._mouseWheelDelta = wheelDelta; };
+gdjs.InputManager.prototype.onMouseWheel = function (wheelDelta) {
+    this._mouseWheelDelta = wheelDelta;
+};
 
 /**
  * Return the mouse wheel delta
  */
-gdjs.InputManager.prototype.getMouseWheelDelta =
-    function() { return this._mouseWheelDelta; };
+gdjs.InputManager.prototype.getMouseWheelDelta = function () {
+    return this._mouseWheelDelta;
+};
 
 /**
  * Get a touch X position
  *
  * @return the touch X position, relative to the game view.
  */
-gdjs.InputManager.prototype.getTouchX = function(identifier) {
-  if (!this._touches.containsKey(identifier))
-    return 0;
+gdjs.InputManager.prototype.getTouchX = function (identifier) {
+    if (!this._touches.containsKey(identifier)) return 0;
 
-  return this._touches.get(identifier).x;
+    return this._touches.get(identifier).x;
 };
 
 /**
@@ -201,73 +212,74 @@ gdjs.InputManager.prototype.getTouchX = function(identifier) {
  *
  * @return the touch Y position, relative to the game view.
  */
-gdjs.InputManager.prototype.getTouchY = function(identifier) {
-  if (!this._touches.containsKey(identifier))
-    return 0;
+gdjs.InputManager.prototype.getTouchY = function (identifier) {
+    if (!this._touches.containsKey(identifier)) return 0;
 
-  return this._touches.get(identifier).y;
+    return this._touches.get(identifier).y;
 };
 
 /**
  * Update and return the array containing the identifiers of all touches.
  *
  */
-gdjs.InputManager.prototype.getAllTouchIdentifiers = function() {
-  gdjs.InputManager._allTouchIds = gdjs.InputManager._allTouchIds || [];
-  gdjs.InputManager._allTouchIds.length = 0;
+gdjs.InputManager.prototype.getAllTouchIdentifiers = function () {
+    gdjs.InputManager._allTouchIds = gdjs.InputManager._allTouchIds || [];
+    gdjs.InputManager._allTouchIds.length = 0;
 
-  for (var id in this._touches.items) {
-    if (this._touches.items.hasOwnProperty(id)) {
-      gdjs.InputManager._allTouchIds.push(parseInt(id, 10));
+    for (var id in this._touches.items) {
+        if (this._touches.items.hasOwnProperty(id)) {
+            gdjs.InputManager._allTouchIds.push(parseInt(id, 10));
+        }
     }
-  }
 
-  return gdjs.InputManager._allTouchIds;
+    return gdjs.InputManager._allTouchIds;
 };
 
-gdjs.InputManager.prototype.onTouchStart = function(identifier, x, y) {
-  this._startedTouches.push(identifier);
-  this._touches.put(identifier, {x : x, y : y});
+gdjs.InputManager.prototype.onTouchStart = function (identifier, x, y) {
+    this._startedTouches.push(identifier);
+    this._touches.put(identifier, { x: x, y: y });
 
-  if (this._touchSimulateMouse) {
-    this.onMouseMove(x, y);
-    this.onMouseButtonPressed(gdjs.InputManager.MOUSE_LEFT_BUTTON);
-  }
+    if (this._touchSimulateMouse) {
+        this.onMouseMove(x, y);
+        this.onMouseButtonPressed(gdjs.InputManager.MOUSE_LEFT_BUTTON);
+    }
 };
 
-gdjs.InputManager.prototype.onTouchMove = function(identifier, x, y) {
-  var touch = this._touches.get(identifier);
-  if (!touch)
-    return;
+gdjs.InputManager.prototype.onTouchMove = function (identifier, x, y) {
+    var touch = this._touches.get(identifier);
+    if (!touch) return;
 
-  touch.x = x;
-  touch.y = y;
+    touch.x = x;
+    touch.y = y;
 
-  if (this._touchSimulateMouse) {
-    this.onMouseMove(x, y);
-  }
+    if (this._touchSimulateMouse) {
+        this.onMouseMove(x, y);
+    }
 };
 
-gdjs.InputManager.prototype.onTouchEnd = function(identifier) {
-  this._endedTouches.push(identifier);
-  if (this._touches.containsKey(
-          identifier)) { // Postpone deletion at the end of the frame
-    this._touches.get(identifier).justEnded = true;
-  }
+gdjs.InputManager.prototype.onTouchEnd = function (identifier) {
+    this._endedTouches.push(identifier);
+    if (this._touches.containsKey(identifier)) {
+        // Postpone deletion at the end of the frame
+        this._touches.get(identifier).justEnded = true;
+    }
 
-  if (this._touchSimulateMouse) {
-    this.onMouseButtonReleased(gdjs.InputManager.MOUSE_LEFT_BUTTON);
-  }
+    if (this._touchSimulateMouse) {
+        this.onMouseButtonReleased(gdjs.InputManager.MOUSE_LEFT_BUTTON);
+    }
 };
 
-gdjs.InputManager.prototype.getStartedTouchIdentifiers =
-    function() { return this._startedTouches; };
+gdjs.InputManager.prototype.getStartedTouchIdentifiers = function () {
+    return this._startedTouches;
+};
 
-gdjs.InputManager.prototype.popStartedTouch =
-    function() { return this._startedTouches.shift(); };
+gdjs.InputManager.prototype.popStartedTouch = function () {
+    return this._startedTouches.shift();
+};
 
-gdjs.InputManager.prototype.popEndedTouch =
-    function() { return this._endedTouches.shift(); };
+gdjs.InputManager.prototype.popEndedTouch = function () {
+    return this._endedTouches.shift();
+};
 
 /**
  * Set if touch events should simulate mouse events.
@@ -276,11 +288,10 @@ gdjs.InputManager.prototype.popEndedTouch =
  * as pressed/released.
  * @param enable {Boolean} true to simulate mouse events, false to disable it.
  */
-gdjs.InputManager.prototype.touchSimulateMouse = function(enable) {
-  if (enable === undefined)
-    enable = true;
+gdjs.InputManager.prototype.touchSimulateMouse = function (enable) {
+    if (enable === undefined) enable = true;
 
-  this._touchSimulateMouse = enable;
+    this._touchSimulateMouse = enable;
 };
 
 /**
@@ -290,32 +301,34 @@ gdjs.InputManager.prototype.touchSimulateMouse = function(enable) {
  * This method should be called in the game loop (see
  * gdjs.RuntimeGame.startGameLoop).
  */
-gdjs.InputManager.prototype.onFrameEnded = function() {
-  // Only clear the ended touches at the end of the frame.
-  for (var id in this._touches.items) {
-    if (this._touches.items.hasOwnProperty(id)) {
-      var touch = this._touches.items[id];
-      if (touch.justEnded) {
-        this._touches.remove(id);
-      }
+gdjs.InputManager.prototype.onFrameEnded = function () {
+    // Only clear the ended touches at the end of the frame.
+    for (var id in this._touches.items) {
+        if (this._touches.items.hasOwnProperty(id)) {
+            var touch = this._touches.items[id];
+            if (touch.justEnded) {
+                this._touches.remove(id);
+            }
+        }
     }
-  }
 
-  this._startedTouches.length = 0;
-  this._endedTouches.length = 0;
-  this._releasedKeys.clear();
-  this._releasedMouseButtons.length = 0;
-  this._mouseWheelDelta = 0;
+    this._startedTouches.length = 0;
+    this._endedTouches.length = 0;
+    this._releasedKeys.clear();
+    this._releasedMouseButtons.length = 0;
+    this._mouseWheelDelta = 0;
 };
 
 /**
  * Return true if the mouse wheel scroll to up
  */
-gdjs.InputManager.prototype.isScrollingUp =
-    function() { return this.getMouseWheelDelta() > 0; };
+gdjs.InputManager.prototype.isScrollingUp = function () {
+    return this.getMouseWheelDelta() > 0;
+};
 
 /**
  * Return true if the mouse wheel scroll to down
  */
-gdjs.InputManager.prototype.isScrollingDown =
-    function() { return this.getMouseWheelDelta() < 0; };
+gdjs.InputManager.prototype.isScrollingDown = function () {
+    return this.getMouseWheelDelta() < 0;
+};
