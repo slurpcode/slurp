8808f4e2066414055586679851aca4c79f2ed0a1
John Bampton <jbampton@gmail.com>
2021-06-25 23:55:32 +1000

    Add pre-commit check `end-of-file-fixer`

--- drivers/fuchsia.rb
+++ drivers/fuchsia.rb
@@ -3,14 +3,11 @@ require 'selenium-webdriver'
 capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
 driver = Selenium::WebDriver.for :firefox, desired_capabilities: capabilities
 
-# driver = Selenium::WebDriver.for :firefox
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-
 begin
   driver.get 'https://fuchsia.surf/'
-  driver.find_element(:tag_name => 'body').send_keys 'matrix', :return
+  driver.find_element(tag_name: 'body').send_keys 'matrix', :return
   sleep(30)
-  driver.find_element(:tag_name => 'body').send_keys 'jedi', :return
+  driver.find_element(tag_name: 'body').send_keys 'jedi', :return
   sleep(60)
 ensure
   driver.quit
