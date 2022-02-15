81e3d0892e3a738ea163d9dca214f1450d2a5536
SM Riad <riad@medinatech.co>
2022-02-15 12:49:34 +0600

    go lint error fix

--- scrapers/go/colly/khan/main.go
+++ scrapers/go/colly/khan/main.go
@@ -29,6 +29,10 @@ func main() {
 
 	c.OnXML("//div[@id='footer']/following::script[contains(.,'prefersReducedMotion')]", func(e *colly.XMLElement) {
 		re, _ := regexp.Compile("points\":(\\d+),\"prefersReducedMotion")
+
+		if re != nil {
+			log.Fatal(re)
+		}
 		res := re.FindAllStringSubmatch(e.Text, -1)
 		if len(res) > 0 {
 			fmt.Println(res[0][1])
