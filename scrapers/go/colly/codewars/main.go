caf74bc218182a8526b4a1091efa72aee16540a9
SM Riad <smriad@gmail.com>
2022-03-24 10:03:51 +0600

    gofmt

--- scrapers/go/colly/codewars/main.go
+++ scrapers/go/colly/codewars/main.go
@@ -1,84 +1,84 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "strings"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
-    "github.com/gocolly/colly/v2"
-    flag "github.com/spf13/pflag"
+	"github.com/gocolly/colly/v2"
+	flag "github.com/spf13/pflag"
 )
 
 // flags
 var (
-    username string
+	username string
 )
 
 func main() {
-    flag.Parse()
-    if flag.NFlag() == 0 {
-        fmt.Printf("Usage: %s [options]\n", os.Args[0])
-        fmt.Println("Options:")
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
-    scrape(username)
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		fmt.Printf("Usage: %s [options]\n", os.Args[0])
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	scrape(username)
 }
 
 func init() {
-    flag.StringVarP(&username, "username", "u", "", "Codewars profile username ")
+	flag.StringVarP(&username, "username", "u", "", "Codewars profile username ")
 }
 
 func scrape(username string) {
-    allowed := "www.codewars.com"
-    url := "https://%s/users/%s"
-    var record []string
-    c := colly.NewCollector(
-        colly.AllowedDomains(allowed),
-        // colly.CacheDir(""),
-    )
+	allowed := "www.codewars.com"
+	url := "https://%s/users/%s"
+	var record []string
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowed),
+		// colly.CacheDir(""),
+	)
 
-    err := c.Limit(&colly.LimitRule{
-        // Filter domains affected by this rule
-        DomainGlob: "codewars.com/*",
-        // Set a delay between requests to these domains
-        Delay: 1 * time.Second,
-        // Add an additional random delay
-        RandomDelay: 1 * time.Second,
-    })
+	err := c.Limit(&colly.LimitRule{
+		// Filter domains affected by this rule
+		DomainGlob: "codewars.com/*",
+		// Set a delay between requests to these domains
+		Delay: 1 * time.Second,
+		// Add an additional random delay
+		RandomDelay: 1 * time.Second,
+	})
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL)
-    })
-    c.OnError(func(_ *colly.Response, err error) {
-        log.Println("Something went wrong:", err)
-    })
-    c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Rank:']/text()", func(e *colly.XMLElement) {
-        record = append(record, e.Text)
-    })
-    c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Honor:']/text()", func(e *colly.XMLElement) {
-        record = append(record, strings.ReplaceAll(e.Text, ",", ""))
-    })
-    c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Leaderboard Position:']/text()", func(e *colly.XMLElement) {
-        record = append(record, strings.ReplaceAll(strings.TrimPrefix(e.Text, "#"), ",", ""))
-    })
-    c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Honor Percentile:']/text()", func(e *colly.XMLElement) {
-        record = append(record, strings.TrimPrefix(e.Text, "Top "))
-    })
-    c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Total Completed Kata:']/text()", func(e *colly.XMLElement) {
-        record = append(record, strings.ReplaceAll(e.Text, ",", ""))
-    })
-    // Start scraping on https://www.codewars.com
-    err = c.Visit(fmt.Sprintf(url, allowed, username))
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+	c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Rank:']/text()", func(e *colly.XMLElement) {
+		record = append(record, e.Text)
+	})
+	c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Honor:']/text()", func(e *colly.XMLElement) {
+		record = append(record, strings.ReplaceAll(e.Text, ",", ""))
+	})
+	c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Leaderboard Position:']/text()", func(e *colly.XMLElement) {
+		record = append(record, strings.ReplaceAll(strings.TrimPrefix(e.Text, "#"), ",", ""))
+	})
+	c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Honor Percentile:']/text()", func(e *colly.XMLElement) {
+		record = append(record, strings.TrimPrefix(e.Text, "Top "))
+	})
+	c.OnXML("//div[@class='stat-box'][ancestor::div[@class='stat-container']/h2/text()='Progress']/div[@class='stat'][b/text()='Total Completed Kata:']/text()", func(e *colly.XMLElement) {
+		record = append(record, strings.ReplaceAll(e.Text, ",", ""))
+	})
+	// Start scraping on https://www.codewars.com
+	err = c.Visit(fmt.Sprintf(url, allowed, username))
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println(record)
+	fmt.Println(record)
 }
--- scrapers/go/colly/github/main.go
+++ scrapers/go/colly/github/main.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "strings"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
-    "github.com/gocolly/colly/v2"
-    "github.com/urfave/cli/v2"
+	"github.com/gocolly/colly/v2"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-    var delay int
-    var username string
-    app := &cli.App{
-        Flags: []cli.Flag{
-            &cli.StringFlag{
-                Name:        "username",
-                Aliases:     []string{"u"},
-                Usage:       "GitHub username",
-                Destination: &username,
-                Required:    true,
-            },
-            &cli.IntFlag{
-                Name:        "delay",
-                Aliases:     []string{"d"},
-                Usage:       "Time to delay in seconds",
-                Destination: &delay,
-                Required:    false,
-                Value:       60,
-            },
-        },
-        Action: func(c *cli.Context) error {
-            return nil
-        },
-    }
-    err := app.Run(os.Args)
-    if err != nil {
-        log.Fatal(err)
-    }
-    record := scrape(username, delay)
-    fmt.Println(record)
+	var delay int
+	var username string
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "username",
+				Aliases:     []string{"u"},
+				Usage:       "GitHub username",
+				Destination: &username,
+				Required:    true,
+			},
+			&cli.IntFlag{
+				Name:        "delay",
+				Aliases:     []string{"d"},
+				Usage:       "Time to delay in seconds",
+				Destination: &delay,
+				Required:    false,
+				Value:       60,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+	record := scrape(username, delay)
+	fmt.Println(record)
 }
 
 func scrape(username string, delay int) []string {
-    time.Sleep(time.Duration(delay) * time.Second)
-    allowed := "github.com"
-    url := "https://%s/%s"
-    var record []string
-    c := colly.NewCollector(
-        colly.AllowedDomains(allowed),
-        // colly.CacheDir(""),
-    )
-    err := c.Limit(&colly.LimitRule{
-        // Filter domains affected by this rule
-        DomainGlob: "github.com/*",
-        // Set a delay between requests to these domains
-        Delay: 1 * time.Second,
-        // Add an additional random delay
-        RandomDelay: 1 * time.Second,
-    })
+	time.Sleep(time.Duration(delay) * time.Second)
+	allowed := "github.com"
+	url := "https://%s/%s"
+	var record []string
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowed),
+		// colly.CacheDir(""),
+	)
+	err := c.Limit(&colly.LimitRule{
+		// Filter domains affected by this rule
+		DomainGlob: "github.com/*",
+		// Set a delay between requests to these domains
+		Delay: 1 * time.Second,
+		// Add an additional random delay
+		RandomDelay: 1 * time.Second,
+	})
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL)
-    })
-    c.OnError(func(_ *colly.Response, err error) {
-        log.Println("Something went wrong:", err)
-    })
-    c.OnHTML("div[class='js-yearly-contributions'] h2[class='f4 text-normal mb-2']", func(e *colly.HTMLElement) {
-        pos := strings.Index(e.Text, " contribution")
-        record = append(record, strings.TrimSpace(strings.ReplaceAll(e.Text[0:pos], ",", "")))
-    })
-    /* GitHub seems to have removed the "count" of repos from profiles - can this be fixed with a crawl ??
-       c.OnXML("//a[contains(@href,'tab=repositories')]/span", func(e *colly.XMLElement) {
-           record = append(record, strings.TrimSpace(e.Text))
-       })
-    */
-    /*c.OnXML("//a[contains(@href,'tab=stars')]/span", func(e *colly.XMLElement) {
-          record = append(record, strings.TrimSpace(e.Text))
-      })
-    */
-    c.OnXML("//a[contains(@href,'tab=followers')]/span", func(e *colly.XMLElement) {
-        record = append(record, strings.TrimSpace(e.Text))
-    })
-    c.OnXML("//a[contains(@href,'tab=following')]/span", func(e *colly.XMLElement) {
-        record = append(record, strings.TrimSpace(e.Text))
-    })
-    err = c.Visit(fmt.Sprintf(url, allowed, username))
-    if err != nil {
-        log.Fatal(err)
-    }
-    return record
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+	c.OnHTML("div[class='js-yearly-contributions'] h2[class='f4 text-normal mb-2']", func(e *colly.HTMLElement) {
+		pos := strings.Index(e.Text, " contribution")
+		record = append(record, strings.TrimSpace(strings.ReplaceAll(e.Text[0:pos], ",", "")))
+	})
+	/* GitHub seems to have removed the "count" of repos from profiles - can this be fixed with a crawl ??
+	   c.OnXML("//a[contains(@href,'tab=repositories')]/span", func(e *colly.XMLElement) {
+	       record = append(record, strings.TrimSpace(e.Text))
+	   })
+	*/
+	/*c.OnXML("//a[contains(@href,'tab=stars')]/span", func(e *colly.XMLElement) {
+	      record = append(record, strings.TrimSpace(e.Text))
+	  })
+	*/
+	c.OnXML("//a[contains(@href,'tab=followers')]/span", func(e *colly.XMLElement) {
+		record = append(record, strings.TrimSpace(e.Text))
+	})
+	c.OnXML("//a[contains(@href,'tab=following')]/span", func(e *colly.XMLElement) {
+		record = append(record, strings.TrimSpace(e.Text))
+	})
+	err = c.Visit(fmt.Sprintf(url, allowed, username))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return record
 }
