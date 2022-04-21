e625607cc91bfa3ca3e4fa2fc18a481d8b1017b2
ihasidul <ihasidul@gmail.com>
2022-04-22 00:25:59 +0600

    Issue: #780 Fix 'scrapers/python/scrapy/github/spider.py' It scrapes 5 stats from github

--- scrapers/python/scrapy/github/spider.py
+++ scrapers/python/scrapy/github/spider.py
@@ -1,18 +1,52 @@
-"""GitHub spider which scrapes five stats."""
+#!/usr/bin/env python3
+"""
+GitHub spider which scrapes five stats.
+Command for running this script:
+    scrapy runspider spider.py
+After running the script it will ask for your github username.Insert your github username.
+"""
 import scrapy
+
+user = input("Enter your github username: ")
 
 
 class GithubSpider(scrapy.Spider):
     """Lets crawl."""
 
     name = "githubspider"
-    start_urls = ["https://github.com/jbampton"]
+    start_urls = ["https://github.com/{}".format(user)]
 
     def parse(self, response):
-        """Grab the 5 stats."""
+        """
+        Grab the 5 stats.
+        Stats it grabs are:
+        - Stars
+        - Followers
+        - Following
+        - Repositories
+        - Contributions in the last year
+        """
         output = {}
-        for repos in response.xpath("//a[contains(@href,'tab=repositories')]/span"):
-            output["repositories"] = repos.css("::text").get().strip()
-        for stars in response.xpath("//a[contains(@href,'tab=stars')]/span"):
-            output["stars"] = stars.css("::text").get().strip()
+        repos = response.xpath("//a[contains(@href,'tab=repositories')]/span")
+        output["repositories"] = repos.css("::text").get().strip()
+        stars = response.xpath("//a[contains(@href,'tab=stars')]/span")
+        output["stars"] = stars.css("::text").get().strip()
+        follower = response.xpath("//a[contains(@href,'tab=followers')]/span")
+        output["follower"] = follower.css("::text").get().strip()
+        following = response.xpath("//a[contains(@href,'tab=following')]/span")
+        output["following"] = following.css("::text").get().strip()
+        contribution_in_the_last_year = response.xpath(
+            "//h2[@class='f4 text-normal mb-2']/text()"
+        )
+        print(contribution_in_the_last_year.get().strip().split("\n")[0])
+        output["contribution_in_the_last_year"] = (
+            contribution_in_the_last_year.get().strip().split("\n")[0]
+        )
+        print(
+            "------------------------------------YOUR STATS------------------------------------"
+        )
+        print(output)
+        print(
+            "----------------------------------------------------------------------------------"
+        )
         yield output
