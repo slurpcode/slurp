4f82351488d01605d62e32dd0dff7a5e9abead09
msaad-khan <msaaddev@gmail.com>
2023-03-28 04:12:18 +0500

    task: updated super linter
    Added python and golang linters to super linter
    
    Added eslint for js ,did not fix errors so ignore them. Will change to eslint airbnb config

---
+++ lint.js
@@ -0,0 +1,27 @@
+const { exec } = require('@actions/exec');
+const core = require('@actions/core');
+
+
+async function run() {
+    try {
+        const pythonCommand = 'python';
+        const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
+        await exec(pythonCommand, pythonArgs);
+
+
+
+        const goCommand = 'golangci-lint';
+        const goArgs = ['run'];
+        await exec(goCommand, goArgs);
+
+
+        console.log('Linting successful');
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error.message);
+    }
+}
+
+(async () => {
+    await run();
+})();
