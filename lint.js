85eb7b9b1a595008fdc99a580a01c86e38fb3493
msaad-khan <msaaddev@gmail.com>
2023-03-30 17:35:15 +0500

    update lint.js

--- lint.js
+++ lint.js
@@ -8,12 +8,17 @@ async function run() {
         const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
         await exec(pythonCommand, pythonArgs);
 
-
+        const nodeCommand = 'npx';
+        const nodeArgs = ['eslint', '.'];
+        await exec(nodeCommand, nodeArgs);
 
         const goCommand = 'golangci-lint';
         const goArgs = ['run'];
         await exec(goCommand, goArgs);
 
+        const rubyCommand = 'rubocop';
+        const rubyArgs = [];
+        await exec(rubyCommand, rubyArgs);
 
         console.log('Linting successful');
     } catch (error) {
