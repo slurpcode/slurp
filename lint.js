d0c2726779cbbbe4639f1b5d68ccfc19b5cbe8a7
msaad-khan <msaaddev@gmail.com>
2023-03-31 18:00:34 +0500

    fixed lint.js linting issues

--- lint.js
+++ lint.js
@@ -3,30 +3,30 @@ const core = require('@actions/core');
 
 
 async function run() {
-    try {
-        const pythonCommand = 'python';
-        const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
-        await exec(pythonCommand, pythonArgs);
+  try {
+    const pythonCommand = 'python';
+    const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
+    await exec(pythonCommand, pythonArgs);
 
-        const nodeCommand = 'npx';
-        const nodeArgs = ['eslint', '.'];
-        await exec(nodeCommand, nodeArgs);
+    const nodeCommand = 'npx';
+    const nodeArgs = ['eslint', '.'];
+    await exec(nodeCommand, nodeArgs);
 
-        const goCommand = 'golangci-lint';
-        const goArgs = ['run'];
-        await exec(goCommand, goArgs);
+    const goCommand = 'golangci-lint';
+    const goArgs = ['run'];
+    await exec(goCommand, goArgs);
 
-        const rubyCommand = 'rubocop';
-        const rubyArgs = [];
-        await exec(rubyCommand, rubyArgs);
+    const rubyCommand = 'rubocop';
+    const rubyArgs = [];
+    await exec(rubyCommand, rubyArgs);
 
-        console.log('Linting successful');
-    } catch (error) {
-        console.log(error);
-        core.setFailed(error.message);
-    }
+    console.log('Linting successful');
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
 }
 
 (async () => {
-    await run();
+  await run();
 })();
