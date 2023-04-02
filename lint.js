8817282c0327da0bf08acae361e4166e3be6cda4
msaad-khan <msaaddev@gmail.com>
2023-04-02 19:52:57 +0500

    god killed the queen

--- lint.js
+++ lint.js
@@ -1,30 +1,23 @@
 const { exec } = require('@actions/exec');
-const core = require('@actions/core');
 
 
 async function run() {
-  try {
-    const pythonCommand = 'python';
-    const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
-    await exec(pythonCommand, pythonArgs);
 
-    const nodeCommand = 'npx';
-    const nodeArgs = ['eslint', '.'];
-    await exec(nodeCommand, nodeArgs);
+  const pythonCommand = 'python';
+  const pythonArgs = ['-m', 'flake8', '--config', './.github/linters/.flake8'];
+  await exec(pythonCommand, pythonArgs);
 
-    const goCommand = 'golangci-lint';
-    const goArgs = ['run'];
-    await exec(goCommand, goArgs);
+  const nodeCommand = 'npx';
+  const nodeArgs = ['eslint', '.'];
+  await exec(nodeCommand, nodeArgs);
 
-    const rubyCommand = 'rubocop';
-    const rubyArgs = [];
-    await exec(rubyCommand, rubyArgs);
+  const goCommand = 'golangci-lint';
+  const goArgs = ['run'];
+  await exec(goCommand, goArgs);
 
-    console.log('Linting successful');
-  } catch (error) {
-    console.log(error);
-    core.setFailed(error.message);
-  }
+  const rubyCommand = 'rubocop';
+  const rubyArgs = [];
+  await exec(rubyCommand, rubyArgs);
 }
 
 (async () => {
