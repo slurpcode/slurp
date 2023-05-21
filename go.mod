efcb13dbbfbaed895a925c2bb806c9571c45a8fc
msaad-khan <msaaddev@gmail.com>
2023-05-21 18:51:40 +0500

    fixed pre-commit errors

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/slurpcode/slurp
 go 1.19
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20230517232016-8abedd933e7a
+	github.com/chromedp/chromedp v0.9.1
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/spf13/pflag v1.0.5
 	github.com/urfave/cli/v2 v2.25.3
@@ -14,16 +16,23 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.15 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
--- scrapers/go/colly/khan/main.go
+++ scrapers/go/colly/khan/main.go
@@ -1,46 +1,133 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"regexp"
+	"time"
 
-	"github.com/gocolly/colly/v2"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	allowed := "www.khanacademy.org"
-	url := "https://%s/profile/%s/"
+	//allowed := "www.khanacademy.org"
+	////allowed := "https://www.khanacademy.org/profile/JohnBampton/"
+	//url := "https://%s/profile/%s/"
 
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: khanacademy.org
-		colly.AllowedDomains(allowed),
+	//// Instantiate default collector
+	//c := colly.NewCollector(
+	//	// Visit only domains: khanacademy.org
+	//	colly.AllowedDomains(allowed),
+	//)
+
+
+	//c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	//	e.Request.Visit(e.Attr("href"))
+	//})
+	//// Before making a request print "Visiting ..."
+	//c.OnRequest(func(r *colly.Request) {
+	//	fmt.Println("Visiting", r.URL.String())
+	//})
+
+	//c.OnError(func(_ *colly.Response, err error) {
+	//	log.Println("Something went wrong:", err)
+	//})
+
+
+	//// Start scraping on https://khanacademy.org
+	////err := c.Visit(fmt.Sprintf(url, allowed, "XSLTGod"))
+	//err := c.Visit(fmt.Sprintf(url, allowed, "JohnBampton"))
+
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+
+	//c := colly.NewCollector()
+
+	// Find and visit all links
+	// c.OnHTML("h2", func(e *colly.HTMLElement) {
+    //    //imgURL := e.Attr("src")
+    //    //fmt.Println("Image URL:", imgURL)
+	//	 fmt.Println(e.Text)
+    //})
+
+
+	//c.OnHTML("h2", func(e *colly.HTMLElement) {
+	//	// Extract the table rows from the table
+	//	fmt.Println(e.Text)
+
+	//})
+
+	//c := colly.NewCollector()
+
+	//// Set the timeout duration to 30 seconds
+	//c.SetRequestTimeout(60 * time.Second)
+
+	//// Specify the URL to scrape
+	//url := "https://www.khanacademy.org/profile/JohnBampton/"
+
+	//// Callback function to handle the scraped data
+	//c.OnHTML("a", func(e *colly.HTMLElement) {
+	//	fmt.Println(e.Text)
+	//})
+
+	//// Error handling callback
+	//c.OnError(func(r *colly.Response, err error) {
+	//	log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	//})
+
+	//// Start the scraping process
+	//err := c.Visit(url)
+	//if err != nil {
+	//	log.Fatal("Error scraping website:", err)
+	//}
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("ignore-certificate-errors", true),
 	)
 
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
+	allocatorCtx, cancel := chromedp.NewExecAllocator(ctx, opts...)
+	defer cancel()
 
-	c.OnXML("//div[@id='footer']/following::script[contains(.,'prefersReducedMotion')]", func(e *colly.XMLElement) {
-		re := regexp.MustCompile("points\":(\\d+),\"prefersReducedMotion")
-		res := re.FindAllStringSubmatch(e.Text, -1)
-		if len(res) > 0 {
-			fmt.Println(res[0][1])
-		} else {
-			fmt.Println("Not found")
-		}
 
-	})
-	// Start scraping on https://khanacademy.org
-	err := c.Visit(fmt.Sprintf(url, allowed, "XSLTGod"))
+	ctx, cancel = chromedp.NewContext(allocatorCtx)
+	defer cancel()
 
+
+	url := "https://www.khanacademy.org/profile/JohnBampton/"
+
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error navigating to the page:", err)
 	}
+
+
+	err = chromedp.Run(ctx,
+		chromedp.Sleep(2*time.Second),
+	)
+	if err != nil {
+		log.Fatal("Error waiting for the page to load:", err)
+	}
+
+
+	var h1Text string
+	err = chromedp.Run(ctx,
+		chromedp.TextContent("h1", &h1Text, chromedp.ByQuery),
+	)
+	if err != nil {
+		log.Fatal("Error extracting h1 text:", err)
+	}
+
+	fmt.Println("h1 text:", h1Text)
 }
