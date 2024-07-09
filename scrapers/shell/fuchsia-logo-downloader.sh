499aad347a8b84760a0d3019d85a6ec3575640d3
John Bampton <jbampton@users.noreply.github.com>
2024-07-09 20:12:12 +1000

    shell: convert indents to tabs (#2691)

--- scrapers/shell/fuchsia-logo-downloader.sh
+++ scrapers/shell/fuchsia-logo-downloader.sh
@@ -4,11 +4,11 @@ sleep 90
 mkdir -p logos
 cd logos || exit
 if [ -z "$1" ]; then
-  j=306
+	j=306
 else
-  j=$1
+	j=$1
 fi
 for ((i = 0; i <= j; i++)); do
-  curl -O https://raw.githubusercontent.com/slurpcode/slurp/master/docs/assets/images/logos/fuchsia-"$i".png
-  sleep 5
+	curl -O https://raw.githubusercontent.com/slurpcode/slurp/master/docs/assets/images/logos/fuchsia-"$i".png
+	sleep 5
 done
