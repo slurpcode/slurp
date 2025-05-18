7697bc76418f5a74691b1404b0f661f017c69934
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-19 00:29:44 +0330

    clean code github/main.go

--- scrapers/go/colly/github/main.go
+++ scrapers/go/colly/github/main.go
@@ -12,8 +12,11 @@ import (
 )
 
 func main() {
-	var delay int
-	var username string
+	var (
+		delay    int
+		username string
+	)
+
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
@@ -28,7 +31,6 @@ func main() {
 			Aliases:     []string{"d"},
 			Usage:       "Time to delay in seconds",
 			Destination: &delay,
-			Required:    false,
 			Value:       60,
 		},
 	}
@@ -37,65 +39,62 @@ func main() {
 		fmt.Println(record)
 		return nil
 	}
-	err := app.Run(os.Args)
-	if err != nil {
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-
 }
 
-// Scrape is the main function that gets all the stats
 func Scrape(username string, delay int) []string {
 	time.Sleep(time.Duration(delay) * time.Second)
-	allowed := "github.com"
-	url := "https://%s/%s"
-	var record []string
-	c := colly.NewCollector(
-		colly.AllowedDomains(allowed),
-		// colly.CacheDir(""),
-	)
-	err := c.Limit(&colly.LimitRule{
-		// Filter domains affected by this rule
-		DomainGlob: "github.com/*",
-		// Set a delay between requests to these domains
-		Delay: 1 * time.Second,
-		// Add an additional random delay
-		RandomDelay: 1 * time.Second,
-	})
 
-	if err != nil {
+	allowedDomain := "github.com"
+	url := fmt.Sprintf("https://%s/%s", allowedDomain, username)
+	var record []string
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowedDomain),
+	)
+
+	if err := c.Limit(&colly.LimitRule{
+		DomainGlob:  "github.com/*",
+		Delay:       1 * time.Second,
+		RandomDelay: 1 * time.Second,
+	}); err != nil {
 		log.Fatal(err)
 	}
+
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
+
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
-	// c.OnHTML("div[class='js-yearly-contributions'] h2[class='f4 text-normal mb-2']", func(e *colly.HTMLElement) {
-	// 	pos := strings.Index(e.Text, " contribution")
-	// 	fmt.Println("Contributions:", e.Text[0:pos])
-	// 	record = append(record, strings.TrimSpace(strings.ReplaceAll(e.Text[0:pos], ",", "")))
-	// })
+
 	c.OnXML("(//a[contains(@href,'tab=repositories')]/span/@title)[1]", func(e *colly.XMLElement) {
 		fmt.Println("Repositories:", e.Text)
 		record = append(record, strings.TrimSpace(e.Text))
 	})
+
 	c.OnXML("(//a[contains(@href,'tab=stars')]/span/@title)[1]", func(e *colly.XMLElement) {
 		fmt.Println("Stars:", e.Text)
 		record = append(record, strings.TrimSpace(e.Text))
 	})
+
 	c.OnXML("//a[contains(@href,'tab=followers')]/span", func(e *colly.XMLElement) {
 		fmt.Println("Followers:", e.Text)
 		record = append(record, strings.TrimSpace(e.Text))
 	})
+
 	c.OnXML("//a[contains(@href,'tab=following')]/span", func(e *colly.XMLElement) {
 		fmt.Println("Following:", e.Text)
 		record = append(record, strings.TrimSpace(e.Text))
 	})
-	err = c.Visit(fmt.Sprintf(url, allowed, username))
-	if err != nil {
+
+	if err := c.Visit(url); err != nil {
 		log.Fatal(err)
 	}
+
 	return record
 }
